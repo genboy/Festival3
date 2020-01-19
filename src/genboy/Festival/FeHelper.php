d3f4fa6996823aeeae46894e2bc456b5afec3f5a
Genboy <genboy@users.noreply.github.com>
2020-01-19 21:53:00 +0100

    test setup with language

--- src/genboy/Festival/FeHelper.php
+++ src/genboy/Festival/FeHelper.php
@@ -5,10 +5,8 @@
  */
 declare(strict_types = 1);
 
-/** src/genboy/Festival2/Helper.php
- *  global helper
- */
 namespace genboy\Festival;
+
 
 class FeHelper {
 
@@ -18,7 +16,15 @@ class FeHelper {
 
         $this->plugin = $owner;
 
+        if(!is_dir($this->plugin->getDataFolder())){
+            @mkdir($this->plugin->getDataFolder());
+		}
+        if( !is_dir($this->plugin->getDataFolder().'resources') ){
+           @mkdir($this->plugin->getDataFolder().'resources'); // add resource folder for backwards compatibility
+		}
+
     }
+
 
 }
 
--- src/genboy/Festival/Festival.php
+++ src/genboy/Festival/Festival.php
@@ -2,18 +2,27 @@
 /** Festival 3.0.0
  * src/genboy/Festival/Festival.php
  * copyright Genbboy 2020
+ *
+ * Setup for Festival 3
+ *
  */
 declare(strict_types = 1);
 
 namespace genboy\Festival;
 
-use genboy\Festival\FeHelper;
-use genboy\Festival\FeCommand;
-use genboy\Festival\FeMenu;
-
+// Language Encoding utf8 (php thingy)
 use neitanod\ForceUTF8\Encoding;
 
+// Festival classes
+use genboy\Festival\FeHelper;
+use genboy\Festival\FeMessenger;
+use genboy\Festival\FeCommand;
+use genboy\Festival\FeMenu;
+use genboy\Festival\FeLanguage as Language;
+
+// PMMP classes
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
 use pocketmine\event\Listener;
 
 use pocketmine\event\player\PlayerJoinEvent;
@@ -23,21 +32,64 @@ use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
+// Festival main class
 class Festival extends PluginBase implements Listener{
 
-    public $menu;
+    public $help,$tell,$cmd,$menu;
 
     public function onEnable() : void{
 
         $this->getServer()->getPluginManager()->registerEvents(new FeListener($this), $this);
 
-        $this->FeCommand = new FeCommand($this);
-        $this->FeHelper = new FeHelper($this);
-        $this->FeMenu = new FeMenu($this);
+        $this->help = new FeHelper($this);
+        $this->menu = new FeMenu($this);
 
-        $this->getLogger()->info( "Genboy copyright 2019" );
+        // check current version, config & data (update old version or new install)
+
+        // load data
+
+        // set default language
+        $this->loadLanguage( "en" );
+
+        $this->getLogger()->info( Language::translate("enabled-console-msg") );
+
+        $this->getLogger()->info( "Genboy copyright 2020" );
 
 	}
+
+    /** load language
+	 * @var plugin config[]
+     * @file resources en.json
+     * @file resources nl.json
+	 * @var obj Language
+	 */
+    public function loadLanguage( $languageCode = false ){
+
+        if( !$languageCode ){
+            $languageCode = 'en';
+        }
+
+        $resources = $this->getResources(); // read files in resources folder
+        foreach($resources as $resource){
+            if($resource->getFilename() === "en.json"){
+              //$text = utf8_encode( file_get_contents($resource->getPathname(), true) ); // json content in utf-8
+              $text = Encoding::toUTF8( file_get_contents($resource->getPathname(), true) );
+              $default = json_decode($text, true); // php decode utf-8
+            }
+            if($resource->getFilename() === $languageCode.".json"){
+              //$text = utf8_encode( file_get_contents($resource->getPathname(), true) );
+              $text = Encoding::toUTF8( file_get_contents($resource->getPathname(), true) );
+              $setting = json_decode($text, true); // php decode utf-8
+            }
+          }
+          if(isset($setting)){
+            $langJson = $setting;
+          }else{
+            $langJson = $default;
+          }
+          new Language($this, $langJson);
+    }
+
 
     /** onJoin
 	 * @param PlayerJoinEvent $event
@@ -89,9 +141,61 @@ class Festival extends PluginBase implements Listener{
 	 */
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
 
-        return $this->FeCommand->onCommand($sender, $command, $label, $args);
+
+        if(!($sender instanceof Player)){
+            $sender->sendMessage( TextFormat::RED . Language::translate("cmd-ingameonly-msg") );
+			return true;
+		}
+
+        if(!isset($args[0])){
+			return false;
+		}
+
+        $playerName = strtolower($sender->getName());
+		$action = strtolower($args[0]);
+		$o = "";
+		switch($action){
+            case "menu":
+                $o = "Menu in development";
+                break;
+
+            default:
+            return false;
+		}
+        $sender->sendMessage($o);
+		return true;
+
 
     }
+
+    /** playerChat
+    * define chat message type
+    * @param string $msg
+    * @param Player $player
+
+    public function playerChat( Player $player, string $msg ) : void {
+
+        $player->sendMessage($msg);
+
+    }
+    */
+
+    /** operatorChat
+    * define chat message type
+    * @param string $msg
+
+    public function operatorChat( string $msg ) : void {
+
+        $players = $this->plugin->getServer()->getOnlinePlayers();
+        foreach($players as $player) {
+            if($player->isOp() ) {
+                //if($sender->getName() === $player->getName() || $to_player->getName() === $player->getName()) {
+                $player->sendMessage($msg);
+            }
+        }
+
+    }
+    */
 
 }
 
@@ -99,8 +203,12 @@ class Festival extends PluginBase implements Listener{
 
 
 Model - config, data and structure - levels, area's, players, records
+
 Controller - interaction View and Model, other plugins
+
 View - commands, event handling, ui
+
+
 
 
 level
@@ -109,6 +217,8 @@ area
 
 player
 
-       form ui XenialDan (https://github.com/thebigsmileXD/customui)
+
+
+form ui XenialDan (https://github.com/thebigsmileXD/customui)
 
 */
